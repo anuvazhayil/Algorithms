c5b6cdf426ea1b4de52267f53b9347947b480171
Anu Vazhayil <anuv.1994@gmail.com>
2017-06-06 14:55:00 +0530

    Takes input as string

--- knightMoves.cpp
+++ knightMoves.cpp
@@ -7,25 +7,25 @@ struct chess{
 };
 
 int findMoves(pair<int, int>curr, pair<int, int>dest){
-    
+
     vector<vector<int> >board(8, vector<int> (8));
     queue<chess>q;
     chess box;
     //possible eight directions
     int dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
     int dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
-    
+
     board[curr.first][curr.second] = 1;
     q.push({curr.first, curr.second, 0});
-    
+
     while(!q.empty()){
         box = q.front();
         q.pop();
-        
+
         if(box.posx == dest.first && box.posy == dest.second){
             return box.dist;
         }
-        
+
         for(int i = 0; i < 8; i++){
             int x = box.posx + dx[i];
             int y = box.posy + dy[i];
@@ -38,10 +38,22 @@ int findMoves(pair<int, int>curr, pair<int, int>dest){
 }
 
 int main(){
+    int T;
+    string a, b;
     pair<int, int>curr, dest;
-    curr = {7,7};
-    dest = {2,2};
-    int res = findMoves(curr, dest);
-    cout<<"\nNo. of minimum moves: "<<res;
+    cin>>T;
+
+    while(T){
+        cin>>a;
+        cin>>b;
+        pair<int, int>curr, dest;
+        curr = {a[0]-'a', a[1]-'1'};
+        dest = {b[0]-'a', b[1]-'1'};
+        cout<<curr.first<<" "<<curr.second;
+        cout<<"\n"<<dest.first<<" "<<dest.second;
+        int res = findMoves(curr, dest);
+        cout<<res<<"\n";
+        T--;
+    }
     return 0;
 }

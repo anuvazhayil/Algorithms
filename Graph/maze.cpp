9204ad59355f419840abefa87fc2f141156cec59
Anu Vazhayil <anuv.1994@gmail.com>
2017-06-09 16:28:10 +0530

    Maze iterative solution

--- Graph/maze.cpp
+++ Graph/maze.cpp
@@ -5,54 +5,84 @@ using namespace std;
 class Cell{
     public:
     int x, y;
-    int value;
+    int val;
     bool visited;
     Cell parent;
-    Cell(int x, int y, int val, bool visited, Cell parent){
-        this.x = x;
-        this.y = y;
-        this.val = val;
-        this.visited = false;
-        this.parent = NULL;
+    Cell(int x, int y, int val, bool visited){
+        this->x = x;
+        this->y = y;
+        this->val = val;
+        this->visited = false;
     }
-}
+};
 
 class Maze{
-    Cell[][] maze;
+    Cell maze[][5];
     public:
-    Maze(int[][] pattern){
-        for(int i = 0; i < pattern.size(); i++){
-            for(int j = 0; j < pattern[0].size(); j++){
-                maze[i][j] = {i, j, pattern[i][j], false, NULL};
+    Maze(int pattern[][5]){
+        int rows = sizeof(pattern)/sizeof(pattern[0]);
+        int cols = sizeof(pattern[0])/sizeof(int);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                Cell *ptr = &maze[i][j].parent;
+                ptr = NULL;
+                maze[i][j] = Cell(i, j, pattern[i][j], false);
+
             }
         }
     }
 
-    Cell* getValue(int x, int y){
+    Cell getValue(int x, int y){
         return maze[x][y];
     }
 
-    void showPath(int srcx, int srcy, int destx, int desty){
-        queue<cell>q;
-        vector<int>result;
-        q.push({srcx, srcy, 1, true, NULL});
-        // BFS
-        while(!q.empty()){
+    stack<Cell> showPath(int srcx, int srcy, int destx, int desty, int rows, int cols){
+        queue<Cell>q;
+        stack<Cell>result;
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, -1, 0, 1};
+        Cell src = getValue(srcx, srcy);
+        src.visited = true;
+        q.push(src);
+        Cell curr;
 
+        while(!q.empty()){
+            int posx, posy;
+            curr = q.front();
+            q.pop();
+
+            if(curr.x == destx && curr.y == desty){
+                break;
+            }
+
+            for(int i = 0; i < 4; i++){
+                posx = curr.x + dx[i];
+                posy = curr.y + dy[i];
+                Cell next = getValue(posx, posy);
+                if(posx >=0 && posx < rows && posy >= 0 && posy < cols && !next.visited && next.val){
+                    next.visited = true;
+                    next.parent = curr;
+                    q.push(next);
+                }
+            }
         }
+        while(curr.parent != NULL){
+            result.push(curr);
+            curr = curr.parent;
+        }
+        return result;
     }
-}
+};
 
 int main(){
-    vector<Cell>result;
-    int[][] pattern = {{1, 0, 1, 1, 1},
-                       {1, 0, 1, 0, 1},
-                       {1, 0, 1, 0, 1},
-                       {1, 1, 1, 1, 1}};
+    stack<Cell>result;
+    int pattern[][5] = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}};
 
     Maze board(pattern);
-    result = board.showPath(1, 0, 2, 2);
-    for(auto ele: result){
+    result = board.showPath(1, 0, 2, 2, 4, 5);
+    while(!result.empty()){
+        Cell ele = result.top();
+        result.pop();
         cout<<"("<<ele.x<<","<<ele.y<<") ->";
     }
     cout<<"Destination Reached!";
